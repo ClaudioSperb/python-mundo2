5e66d2dd3587ee01d9cebfd7d5eae032f5729d77
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-23 16:08:57 -0300

    Create desafio036.py

---
+++ Desafios/desafio036.py
@@ -0,0 +1,23 @@
+from time import sleep
+from colorama import init, Fore
+
+nomeCliente = str(input('Digite o nome do cliente: ')).strip().upper()
+valorSalario = float(input('Digite o salário atualizado do comprador: '))
+valorCasa = float(input('Digite o valor da casa: '))
+tempoPrestação = int(input('Digite quantas parecelas desejadas: '))
+anosParcela = tempoPrestação / 12
+valorPrestação = valorCasa / tempoPrestação
+validando = valorSalario * (30 / 100)
+salarioValidado = valorSalario - validando
+print(f'O valor a ser financiado é de {valorCasa}.')
+print('_-_' * 30)
+print(f'A quantidade de parcelas serão de {tempoPrestação} vezes - {anosParcela:.2f} anos')
+print('_-_' * 30)
+print(f'O valor da parcela é de R${valorPrestação:.2f} reais')
+print('Analisando Crédito . . .')
+print('_-_' * 30)
+sleep(2)
+if valorPrestação > salarioValidado:
+    print(f'{Fore.LIGHTRED_EX}Crédito NÃO APROVADO{Fore.RESET} Verifique a documentação.')
+else:
+    print(f'Parabens {nomeCliente} !!! , seu credito foi {Fore.LIGHTGREEN_EX}APROVADO{Fore.RESET}')
