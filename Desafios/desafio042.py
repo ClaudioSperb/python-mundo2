552b40c1f06d791a3ddd98b14d81de601144ff06
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-26 14:48:54 -0300

    Update desafio042.py

--- Desafios/desafio042.py
+++ Desafios/desafio042.py
@@ -8,18 +8,18 @@ print('')
 print('Verificando as medidas ...')
 sleep(1.5)
 print('')
-if ladoA + ladoB > ladoC:
+if ladoA + ladoB > ladoC and ladoA + ladoC > ladoB and ladoB + ladoC > ladoA:
     print(f'Com os numeros digitados {Fore.LIGHTGREEN_EX}É POSSÍVEL{Fore.RESET} formar um triângulo')
 else:
     print(f'Com os numeros digitados, {Fore.LIGHTRED_EX}NÃO È POSSIVEL{Fore.RESET} formar um triângulo')
 
 print('_--_' * 10)
 
-if ladoA == ladoB == ladoC and ladoA + ladoB > ladoC:
+if ladoA == ladoB == ladoC and ladoA + ladoB > ladoC and ladoA + ladoC > ladoB and ladoB + ladoC > ladoA:
     print(f'{Fore.LIGHTCYAN_EX}Triângulo Equilatero{Fore.RESET}, pois todos os Lados são iguais')
-elif ladoA == ladoB and ladoA == ladoC and ladoB == ladoC and ladoA + ladoB > ladoC:
+elif ladoA == ladoB and ladoA == ladoC and ladoB == ladoC and ladoA + ladoB > ladoC and ladoA + ladoC > ladoB and ladoB + ladoC > ladoA:
     print(f'{Fore.LIGHTCYAN_EX}Triângulo Isósceles{Fore.RESET}, pois possuem 2 lados iguais')
-elif ladoA != ladoB and ladoC and ladoA + ladoB > ladoC:
+elif ladoA != ladoB and ladoC and ladoA + ladoB > ladoC and ladoA + ladoC > ladoB and ladoB + ladoC > ladoA:
     print(f'{Fore.LIGHTCYAN_EX}Triângulo Escaleno{Fore.RESET}, pois tem os 3 lados diferentes')
 
 
