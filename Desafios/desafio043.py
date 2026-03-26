e59d879e4713c0508b2806eb448fd58a697b845b
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-26 16:14:03 -0300

    Update desafio043.py

--- Desafios/desafio043.py
+++ Desafios/desafio043.py
@@ -6,12 +6,12 @@ altura = float(input('Digite sua altura: '))
 imc = peso / (altura ** 2)
 #print(f'{imc:.2f}')
 if imc < 18.5:
-    print(f'{nome} seu IMC é de {imc:.2f} , você está abaixo do peso.')
+    print(f'{nome} seu IMC é de {Fore.LIGHTCYAN_EX}{imc:.2f}{Fore.RESET} , você está {Fore.LIGHTYELLOW_EX}ABAIXO DO PESO.{Fore.RESET}')
 elif imc < 25:
-    print(f'{nome} seu IMC é de {imc:.2f} , você está com peso ideal.')
+    print(f'{nome} seu IMC é de {Fore.LIGHTCYAN_EX}{imc:.2f}{Fore.RESET} , você está com {Fore.LIGHTGREEN_EX}PESO IDEAL.{Fore.RESET}')
 elif imc < 30:
-    print(f'{nome} seu IMC é de {imc:.2f} , você está com sobrepeso.')
+    print(f'{nome} seu IMC é de {Fore.LIGHTCYAN_EX}{imc:.2f}{Fore.RESET} , você está com {Fore.LIGHTYELLOW_EX}SOBREPESO.{Fore.RESET}')
 elif imc < 40:
-    print(f'{nome} seu IMC é de {imc:.2f} , você está com obesidade grau 1.')
+    print(f'{nome} seu IMC é de {Fore.LIGHTCYAN_EX}{imc:.2f}{Fore.RESET} , você está com {Fore.LIGHTRED_EX}OBESIDADE GRAU 1.{Fore.RESET}')
 else:
-    print(f'{nome} seu IMC é de {imc:.2f} , você está com obesidade grau 2.')
+    print(f'{nome} seu IMC é de {Fore.LIGHTCYAN_EX}{imc:.2f}{Fore.RESET} , você está com {Fore.LIGHTRED_EX}OBESIDADE GRAU 2.{Fore.RESET}')
