1b822b329ab53c84c02933dbd53d52587fa09183
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-27 17:22:16 -0300

    Update desafio037.py

--- Desafios/desafio037.py
+++ Desafios/desafio037.py
@@ -13,9 +13,9 @@ numBin = bin(num)
 numHex = hex(num)
 numOctal = oct(num)
 if escolha == '1':
-    print(f'A converão de {num} em Binario fica {Fore.LIGHTGREEN_EX}{numBin}')
+    print(f'A converão de {num} em Binario fica {Fore.LIGHTGREEN_EX}{numBin[2:]}')
 elif escolha == '2':
-    print(f'A conversão de {num} em Octadecimal fica {Fore.LIGHTRED_EX}{numOctal}')
+    print(f'A conversão de {num} em Octadecimal fica {Fore.LIGHTRED_EX}{numOctal[2:]}')
 else:
-    print(f'A conversão de {num} para Hexadecimal fica {Fore.LIGHTBLUE_EX}{numHex}')
+    print(f'A conversão de {num} para Hexadecimal fica {Fore.LIGHTBLUE_EX}{numHex[2:]}')
 
