df7c8867fb94431e763415acd7dd380d2b4b0d60
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-01 12:53:53 -0300

    Create aula013.py

---
+++ Aulas/aula013.py
@@ -0,0 +1,13 @@
+#PRATICANDO FOR
+'''numTabuada = int(input('Digite um número: '))
+n = int(input('Tabuada do: '))
+for c in range(0, 11):
+    print(n * c)
+
+frutas = ['Maça', 'Uva', 'Pêssego', 'Mamão']
+for c in (frutas):
+    print(c)'''
+
+n = int(input('Digite um numero: '))
+for c in range(0, 101, n):
+    print(c)
