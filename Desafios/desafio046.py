dd81f27c9e2757988438d8d594b19d037c036b85
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-02 10:49:28 -0300

    Update desafio046.py

--- Desafios/desafio046.py
+++ Desafios/desafio046.py
@@ -3,11 +3,11 @@ from colorama import Fore
 print(f'{Fore.LIGHTGREEN_EX}========= CONTAGEM REGRESSIVA =========={Fore.RESET}')
 print('')
 print('PARA INICIAR A CONTAGEM DIGITE [0]: ')
-contagem = int(input('PODE DIGITAR AGORA: '))
+contagem = int(input('PODE DIGITAR AGORA: 💣  '))
 if contagem == 0 :
     for i in range(10, 0, -1):
         sleep(1)
         print(i)
-    print(f'{Fore.LIGHTYELLOW_EX}🎆🧨 FELIZ ANO NOVO !!!! 🧨🎆{Fore.RESET} ')
+    print(f'{Fore.LIGHTYELLOW_EX}🧨💣 BUMMMMMMMM !!!! 💣🧨{Fore.RESET} ')
 else:
     print('CONTAGEM CANCELADA')
