94820df53490bbd1678f690f4931bafd0cd77b1b
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-08 09:58:01 -0300

    Update desafio052.py

--- Desafios/desafio052.py
+++ Desafios/desafio052.py
@@ -1,5 +1,11 @@
 from time import sleep
+from colorama import Fore
 print(f'{10 * '='} TESTANDO NÚMEROS PRIMOS {10 * '='}')
 num = int(input('Digite um numero inteiro: '))
 sleep(0.5)
 print('Valindando número .... ')
+for c in range (1, num + 1):
+    if num % c == 0:
+        print(f'{Fore.LIGHTGREEN_EX}{c}{Fore.RESET}', end=' ')
+    else:
+        print(f'{Fore.LIGHTRED_EX}{c}{Fore.RESET}', end=' ')
