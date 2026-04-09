32d3712814b26d7bb95bc8a9a8d694e7f7ba758a
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-09 10:33:25 -0300

    Update desafio050.py

--- Desafios/desafio050.py
+++ Desafios/desafio050.py
@@ -2,6 +2,6 @@ from colorama import Fore
 print(f'{10 * '='} {Fore.CYAN}DESAFIO 050{Fore.RESET} {10 * '='}')
 soma = 0
 for num in range (0, 6):
-    n = int(input('Digite um numero: '))
+    n = int(input(f'Digite o {num + 1}º numero: '))
     if n % 2 == 0: soma += n
 print(f'A soma dos números pares ficou {Fore.LIGHTBLUE_EX}{soma}{Fore.RESET}')
