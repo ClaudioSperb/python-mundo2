fb880bc53527068ecfc887b62d442ea4a805c69d
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-10 16:06:07 -0300

    Update praticando.py

--- Aulas/praticando.py
+++ Aulas/praticando.py
@@ -1,3 +1,12 @@
+from time import sleep
 dados = ['Claudio', 'Josiane','Elton','Briana','Lucas', 'Carlos', 'Eduardo']
-for lista in range(len(dados)):
-    print(lista)
+for nomes in dados:
+    print(nomes)
+    sleep(0.5)
+# print(dados[0])
+# print(dados[1])
+# print(dados[2])
+# print(dados[3])
+# print(dados[4])
+# print(dados[5])
+# print(dados[6])
