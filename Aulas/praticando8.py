813dc2a866572e7185b46ff4260382d4edc266fd
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-13 14:31:18 -0300

    Update praticando8.py

--- Aulas/praticando8.py
+++ Aulas/praticando8.py
@@ -10,8 +10,8 @@ for i in range(0, 3):
 for i in range(0, 3):
     numEscolha = int(input('Digite um numero: '))
     valores_linha3.append(numEscolha)
-matriz = [valores_linha1, valores_linha2, valores_linha3]
     
-print(f'{valores_linha1}')
-print(f'{valores_linha2}')
-print(f'{valores_linha3}')
+matriz = [valores_linha1, valores_linha2, valores_linha3]
+
+for linha in matriz:
+    print(f'{valores_linha1}\n{valores_linha2}\n{valores_linha3}')
