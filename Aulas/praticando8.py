6acb4fd2fd8c2e991d24e253b8a72db004d05a40
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-13 16:04:41 -0300

    Update praticando8.py

--- Aulas/praticando8.py
+++ Aulas/praticando8.py
@@ -1,13 +1,15 @@
 valores_linha1 = []
 valores_linha2 = []
 valores_linha3 = []
-
+print('====== FORMANDO A PRIMEIRA LINHA ======')
 for i in range(0, 3):
     numEscolha = int(input('Digite um numero: '))
     valores_linha1.append(numEscolha)
+print('====== FORMANDO A SEGUNDA LINHA ======')
 for i in range(0, 3):
     numEscolha = int(input('Digite um numero: '))
     valores_linha2.append(numEscolha)
+print('====== FORMANDO A TERCEIRA LINHA ======')
 for i in range(0, 3):
     numEscolha = int(input('Digite um numero: '))
     valores_linha3.append(numEscolha)
@@ -15,8 +17,9 @@ for i in range(0, 3):
 matriz = [valores_linha1, valores_linha2, valores_linha3]
 
 print("\n--- MATRIZ FORMATADA ---")
+print('========================')
 for linha in matriz:
     print(linha)
-    
+print(' ')
 soma = sum(valores_linha1 + valores_linha2 + valores_linha3)
 print(f'O valor total dos valores da matriz é {soma}')
