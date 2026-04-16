2bae6c63bea8e355a01c4643ad28c7ecd8ddc964
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-16 17:05:09 -0300

    Update desafio059.py

--- Desafios/desafio059.py
+++ Desafios/desafio059.py
@@ -1,4 +1,5 @@
 from colorama import Fore
+from time import sleep
 print(f'{15 * '='} CRIANDO UM MENU DE OPÇÕES {15 * '='}')
 num1 = int(input('Digite um número: '))
 num2 = int(input('Digite outro Número: '))
@@ -18,15 +19,28 @@ while opção != 5:
         
     if opção == 1:
         soma = num1 + num2
-        print(soma)
+        print(f'Você escolheu somar os valores: ')
+        print('somando . . .')
+        sleep(1)
+        print(f'O resultado da soma é - {soma}')
     elif opção == 2:
         mult = num1 * num2
-        print(mult)
+        print(f'Você escolheu multiplicar os valores: ')
+        print('Multiplicando . . .')
+        sleep(1)
+        print(f'O resultado da Multiplicação é {mult}')
     elif opção == 3:
+        print('AGUARDE .... ')
+        sleep(1)
         num1 = int(input('Digite um número: '))
         num2 = int(input('Digite outro Número: '))
     elif opção == 4:
         numeros.append(num1)
         numeros.append(num2)
         maiorNumero = max(numeros)
-        print(maiorNumero)
+        print('ANALISANDO O MAIOR NUMERO DIGITADO >>> ')
+        sleep(1)
+        print(f'O maior número digitado foi o {maiorNumero}')
+    elif opção == 5:
+        print('FINALIZANDO O PROGRAMA . . .')
+        sleep(1)
