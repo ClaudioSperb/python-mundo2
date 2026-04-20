06cecbd8adfbbf7617151b089430a31f2bed65c3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-04-20 12:33:51 -0300

    Create desafio063.py

---
+++ Desafios/desafio063.py
@@ -0,0 +1,16 @@
+from time import sleep
+print(f'{10 * '='}SEQUENCIA DE FIBONACCI{10 * '='}')
+numero = int(input('Quantos termos você quer mostrar:  '))
+print(f'{50 * '-'}')
+t1 = 0
+t2 = 1
+cont = 3
+print('GERANDO SEQUENCIA >>> ')
+sleep(1)
+print(f'{t1} -> {t2}', end=' ')
+while cont <= numero:
+    t3 = t1 + t2
+    cont += 1
+    t1 = t2
+    t2 = t3
+    print(f' -> {t3}', end=' ')
