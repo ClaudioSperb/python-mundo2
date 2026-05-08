8160138bd97a3681979e167c99a041881046a8df
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-05-08 20:16:41 -0300

    Update desafio069.py

--- Desafios/desafio069.py
+++ Desafios/desafio069.py
@@ -1,2 +1,41 @@
 from time import sleep
 from colorama import Fore
+total_pessoas_maior = total_mulheres = total_mulheres_menor = total_homens = 0
+while True:
+    print(f'{15 * '='} CADASTRO EM ANDAMENTO {15 * '='}')
+    idade = int(input('Digite sua idade: '))
+    sexo = str(input('Digite seu Sexo [F / M]: ')).upper().strip()
+    pergunta = str(input('Você quer continuar [S / N]: ')).upper().strip()
+
+    if pergunta == 'N' and idade >= 18 and sexo == 'M':
+        print(f'{Fore.RED}FINALIZANDO O CADASTRO ...{Fore.RESET}')
+        sleep(1)
+        total_pessoas_maior += 1
+        total_homens += 1
+        break
+    elif pergunta == 'N' and idade >= 18 and sexo == 'F':
+        print(f'{Fore.RED}FINALIZANDO O CADASTRO ...{Fore.RESET}')
+        sleep(1)
+        total_pessoas_maior += 1
+        total_mulheres += 1
+        break
+    elif pergunta == 'N' and idade < 18 and sexo == 'F':
+        print(f'{Fore.RED}FINALIZANDO O CADASTRO ...{Fore.RESET}')
+        sleep(1)
+        total_mulheres_menor += 1
+        break
+
+    #VALIDANDO A MAIORIDADE
+    if idade >= 18:
+        total_pessoas_maior += 1
+    #VALIDANDO SE É DO SEXO MASCULINO
+    if sexo == 'M':
+        total_homens += 1
+
+    #VALIDANDO SE É MENOR E DO SEXO FEMININO
+    if sexo == 'F' and idade < 18:
+        total_mulheres_menor += 1
+
+print(f'O total de pessoas cadastrados de maior idade foi de {total_pessoas_maior}.')
+print(f'O total de pessoas do {Fore.LIGHTBLUE_EX}sexo Masculino{Fore.RESET} cadastrados foram de {total_homens} homens')
+print(f'O total de pessoas do {Fore.LIGHTMAGENTA_EX}sexo Feminino{Fore.RESET} menores de 18 anos cadastrados foram de {total_mulheres_menor} mulheres')
