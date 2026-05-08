54489e19c24d8d65edb26cb529f4be5e971c5df0
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-05-07 21:50:00 -0300

    Update desafio068.py

--- Desafios/desafio068.py
+++ Desafios/desafio068.py
@@ -2,6 +2,7 @@ from random import randint
 from time import sleep
 print(f'{10 * '='} JOGO DO PAR OU ÍMPAR {10 * '='}')
 numHumano = 0
+vitoria = 0
 while True:
     numHumano = int(input('Digite um número: '))
     resHumano = str(input('Digite a opção desejada: [P / I]: ')).upper().strip()
@@ -11,10 +12,12 @@ while True:
         print(f'Você escolheu [ PAR ] e o numero {numHumano} e o computador o numero {numComputador}')
         print(f'A soma dos numeros deu {soma} é PAR')
         print('VOCÊ VENCEU !!!! ')
+        vitoria += 1
     elif soma % 2 == 1 and resHumano == 'I':
         print(f'Você escolheu [ IMPAR ] e o numero {numHumano} e o computador o numero {numComputador}')
         print(f'A soma dos numeros deu {soma} é ÍMPAR')
         print('VOCÊ VENCEU !!!! ')
+        vitoria += 1
     elif soma % 2 == 0 and resHumano == 'I':
         print(f'Você escolheu [ IMPAR ] e o numero {numHumano} e o computador o numero {numComputador}')
         print(f'A soma dos numeros deu {soma} é PAR')
@@ -27,4 +30,5 @@ while True:
         print(f'{48 * '='}')
         print('GAME OVER - VOCÊ PERDEU !!!! ')
         break
-print('FIM')
+print(f'Você ganhou {vitoria} Vezes !!! ')
+print('FIM DO JOGO')
