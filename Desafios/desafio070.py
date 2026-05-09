b5cbbca2bf5e1355dd3f9e7d8fb739260f8a60b4
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-05-09 08:14:53 -0300

    Create desafio070.py

---
+++ Desafios/desafio070.py
@@ -0,0 +1,21 @@
+from colorama import Fore
+from time import sleep
+print(f'{15 * '~'} MERCADO DO CLAUDIO {15 * '~'}')
+print('Seja Bem vindo ao nosso Mercado')
+print('')
+lista_Produtos = []
+Valores_Produtos = []
+while True:
+    print(f'{15 * '='} PAGINA DE COMPRA {15 * '='}')
+    produto = str(input('Nome do Produto: ')).title().strip()
+    valor = float(input('Valor do Produto: R$'))
+    res = str(input('Continuar comprando [S / N] ? ')).capitalize().strip()
+    lista_Produtos.append(produto)
+    Valores_Produtos.append(valor)
+    if res == 'N':
+        print(f'{Fore.LIGHTRED_EX}ENCERRANDO ...{Fore.RESET}')
+        sleep(1)
+        break
+    
+print(f'{lista_Produtos}')
+print(f'{Valores_Produtos}')
