4ecb70d5a5a1a75f46a791dea1ca1798d58fb8b3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-05-08 21:56:43 -0300

    Update desafio069.py

--- Desafios/desafio069.py
+++ Desafios/desafio069.py
@@ -1,11 +1,15 @@
 from time import sleep
 from colorama import Fore
 total_pessoas_maior = total_mulheres = total_mulheres_menor = total_homens = 0
-while True:
+sexo = ''
+pergunta = ''
+while pergunta != 'N':
     print(f'{15 * '='} CADASTRO EM ANDAMENTO {15 * '='}')
     idade = int(input('Digite sua idade: '))
-    sexo = str(input('Digite seu Sexo [F / M]: ')).upper().strip()
-    pergunta = str(input('Você quer continuar [S / N]: ')).upper().strip()
+    while sexo != 'M' and sexo != 'F':
+        sexo = str(input('Digite seu Sexo [F / M]: ')).upper().strip()
+    while pergunta != 'S' and pergunta != 'N':
+        pergunta = str(input('Você quer continuar [S / N]: ')).upper().strip()
 
     if pergunta == 'N' and idade >= 18 and sexo == 'M':
         print(f'{Fore.RED}FINALIZANDO O CADASTRO ...{Fore.RESET}')
@@ -35,7 +39,7 @@ while True:
     #VALIDANDO SE É MENOR E DO SEXO FEMININO
     if sexo == 'F' and idade < 18:
         total_mulheres_menor += 1
-
+print(f'{86 * '='}')
 print(f'O total de pessoas cadastrados de maior idade foi de {total_pessoas_maior}.')
 print(f'O total de pessoas do {Fore.LIGHTBLUE_EX}sexo Masculino{Fore.RESET} cadastrados foram de {total_homens} homens')
 print(f'O total de pessoas do {Fore.LIGHTMAGENTA_EX}sexo Feminino{Fore.RESET} menores de 18 anos cadastrados foram de {total_mulheres_menor} mulheres')
