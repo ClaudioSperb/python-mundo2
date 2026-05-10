534ce1157eb674278f1046393f75a93bbee2ca9f
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-05-09 22:28:08 -0300

    Update desafio070.py

--- Desafios/desafio070.py
+++ Desafios/desafio070.py
@@ -7,20 +7,31 @@ lista_Produtos = []
 lista_valores =[]
 Valores_Produtos = 0
 soma_produtos = 0
-while True:
+valor_maior = 0
+res = ''
+while res != 'N':
     print(f'{15 * '='} PAGINA DE COMPRA {15 * '='}')
     produto = str(input('Nome do Produto: ')).title().strip()
     valor = float(input('Valor do Produto: R$'))
-    res = str(input('Continuar comprando [S / N] ? ')).title().strip()
+    res = ''
+    while res != 'S' and res != 'N':
+        res = str(input('Continuar comprando [S / N] ? ')).title().strip()
     soma_produtos += valor
     lista_Produtos.append(produto)
     lista_valores.append(valor)
 
     if res == 'N':
+        valor_maior += 1
         print(f'{Fore.LIGHTRED_EX}ENCERRANDO ...{Fore.RESET}')
         sleep(1)
         break
 
-print(f'{soma_produtos}')
+    if valor > 1000:
+        valor_maior += 1
+valor_barato = min(lista_valores)
+
+print(f'{valor_barato}')
+print(f'O valor total da sua compra foi de R${soma_produtos:.2f}')
+print(f'Na sua compra tem {valor_maior} produtos a cima de R$1.000,00 reais')
 print(f'{lista_Produtos}')
 print(f'{lista_valores}')
