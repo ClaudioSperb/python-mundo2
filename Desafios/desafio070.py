1e4be1a57492dc554b0450d052fd47989c712437
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-05-11 10:45:46 -0300

    Update desafio070.py

--- Desafios/desafio070.py
+++ Desafios/desafio070.py
@@ -1,37 +1,39 @@
 from colorama import Fore
 from time import sleep
-print(f'{15 * '~'} MERCADO DO CLAUDIO {15 * '~'}')
-print('Seja Bem vindo ao nosso Mercado')
-print('')
-lista_Produtos = []
-lista_valores =[]
-Valores_Produtos = 0
-soma_produtos = 0
+
+print(f'{15 * "~"} MERCADO DO CLAUDIO {15 * "~"}')
+print('Seja Bem vindo ao nosso Mercado\n')
+
+soma = 0
 valor_maior = 0
-res = ''
-while res != 'N':
-    print(f'{15 * '='} PAGINA DE COMPRA {15 * '='}')
-    produto = str(input('Nome do Produto: ')).title().strip()
+menor_preco = 0
+produto_barato = ''
+cont = 0 # Contador para saber qual é o primeiro produto
+
+while True:
+    produto = str(input('Digite o nome do Produto: ')).title().strip()
     valor = float(input('Valor do Produto: R$'))
-    res = ''
-    while res != 'S' and res != 'N':
-        res = str(input('Continuar comprando [S / N] ? ')).title().strip()
-    soma_produtos += valor
-    lista_Produtos.append(produto)
-    lista_valores.append(valor)
+    cont += 1
+    soma += valor
 
-    if res == 'N':
-        valor_maior += 1
-        print(f'{Fore.LIGHTRED_EX}ENCERRANDO ...{Fore.RESET}')
-        sleep(1)
-        break
-
+    # Lógica para contar produtos acima de R$1000
     if valor > 1000:
         valor_maior += 1
 
-#PEGANDO OS PRODUTOS E VALORES
-valor_barato = min(zip(lista_Produtos, lista_valores))
+    # Lógica para descobrir o mais barato
+    if cont == 1 or valor < menor_preco:
+        menor_preco = valor
+        produto_barato = produto
 
-print(f'O valor total da sua compra foi de R${soma_produtos:.2f}')
-print(f'Na sua compra tem {valor_maior} produtos a cima de R$1.000,00 reais')
-print(f'O item mais barato da sua compra foi {valor_barato[0]}.')
+    off = ' '
+    while off not in 'SN':
+        off = str(input('Deseja continuar comprando [S/N]: ')).upper().strip()[0]
+    
+    if off == 'N':
+        break
+
+print("-" * 40)
+print(f'O total da compra foi R${soma:.2f}')
+print(f'Temos {valor_maior} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {produto_barato} que custa R${menor_preco:.2f}')
+print('FIM')
