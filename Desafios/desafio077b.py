c1152a4eb4ff5c66c110a1e04c28471778d7ba11
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-01 11:32:13 -0300

    Update desafio077b.py

--- Desafios/desafio077b.py
+++ Desafios/desafio077b.py
@@ -1,3 +1,13 @@
+from colorama import Fore
+print(f'{Fore.LIGHTGREEN_EX}{25 * '~='}{Fore.RESET}')
 engates = ('GOL', 'CELTA', 'CIVIC', 'SAVEIRO', 'UNO',
            'CORSA', 'FUSION', 'CERATO', 'T-CROSS', 'NIVUS',
-           'LIVINA', 'LOGUS', 'TROLLER', 'TIGGO', 'RENEGADE')
+           'LIVINA', 'LOGUS', 'TROLLER', 'TIGGO', 'RENEGADE',
+           'CRETA', 'COMMANDER', 'IX-35', 'LOGAN', 'COBALT')
+for palavra in engates:
+    print(f'A palavra {Fore.CYAN}{palavra.upper()}{Fore.RESET} tem as vogais -> ', end='')
+    for letra in palavra:
+        if letra in 'AEIOUaeiou':
+            print(f'{Fore.RED}{letra}{Fore.RESET}', end=' ')
+    print()
+print(f'{Fore.GREEN}{25 * '~='}{Fore.RESET}')
