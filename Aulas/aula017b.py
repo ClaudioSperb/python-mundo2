02ccab6cbb10813fb1eb2675a4655677d4621717
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-01 23:24:01 -0300

    atualização

---
+++ Aulas/aula017b.py
@@ -0,0 +1,5 @@
+print('TRABALHANDO COM LISTAS')
+engates = ['Gol', 'Celta', 'Corsa', 'Uno']
+if 'Gol' in engates:
+    engates.remove('Gol')
+print(engates)
---
+++ Aulas/aula017c.py
@@ -0,0 +1,9 @@
+print('TRABALHANDO COM LISTAS')
+valores = list(range(4, 11))
+print(valores)
+
+valores_a = [4, 7, 8, 2, 1, 5]
+print(valores_a)
+valores_a.sort()
+print(valores_a)
+print(len(valores)) #Começando sempre pelo indice 1
