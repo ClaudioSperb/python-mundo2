d6ed1fadb2a5d79f6b4e7a20ed1e163adf80b2c3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-02 15:25:15 -0300

    atualização

--- Aulas/aula017e.py
+++ Aulas/aula017e.py
@@ -1,15 +1,11 @@
 numeros = []
-numeros.append(1)
-numeros.append(5)
-numeros.append(9)
 
 for c in numeros:
     print(f'{c}...')
     
+for cont in range(0, 5):
+    numeros.append(int(input('Digite um Numero: ')))
+    
 for p, c in enumerate(numeros):
     print(f'Na posição {p} esta o numero {c}')
 print('FIM')
-
-for cont in range(0, 5):
-    numeros.append(int(input('Digite um Numero: ')))
-print(f'Na posição {p} esta o numero {c}')
