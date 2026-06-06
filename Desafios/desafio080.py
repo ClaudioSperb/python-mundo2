320dacfd2be7d97ff08ec784958e8512586749a0
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-06 11:49:07 -0300

    Update desafio080.py

--- Desafios/desafio080.py
+++ Desafios/desafio080.py
@@ -2,10 +2,16 @@ print(f'{40 * '='}')
 print('COLOCANDO TUDO EM ORDEM'.center(40))
 print(f'{40 * '='}')
 lista_numeros = []
+lista_maior = []
+lista_menor = []
 for c in range(5):
     num = int(input('Digite um numero: '))
     lista_numeros.append(num)
     maior_numero = max(lista_numeros)
+    menor_numero = min(lista_numeros)
     if num > maior_numero:
-        lista_numeros.insert(-1, num)
+        lista_maior.insert(num, -1)
+    elif num < maior_numero:
+        lista_menor.insert(0, num)
+print(lista_maior, lista_menor)
         
