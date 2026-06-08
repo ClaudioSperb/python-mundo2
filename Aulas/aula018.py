ecd4221b87ab31cc7190232d031ef37fb076f530
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-07 23:00:07 -0300

    Create aula018.py

---
+++ Aulas/aula018.py
@@ -0,0 +1,12 @@
+print('Listas Compostas')
+cadastro = []
+pessoa1 = ['Claudio', 36]
+pessoa2 = ['Josiane', 32]
+pessoa3 = ['Briana', 3]
+
+cadastro.append(pessoa1)
+cadastro.append(pessoa2)
+cadastro.append(pessoa3)
+
+print(cadastro)
+print(cadastro[2][0])
