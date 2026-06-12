858a647576236f6b46e4cd58680e9e7428fa9c59
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-12 13:33:18 -0300

    Update desafio084.py

--- Desafios/desafio084.py
+++ Desafios/desafio084.py
@@ -8,17 +8,22 @@ lista_leves = []
 
 while True:
     nome = str(input('Qual seu nome: '))
-    peso = float(input('Qual seu peso: Kg'))
+    peso = float(input('Qual seu peso: '))
     res = str(input('Quer continuar [S / N]: ')).upper().strip()
     lista_usuarios.append(nome)
     lista_usuarios.append(peso)
     
-    
-    
+    if peso >= 100 :
+        lista_pesados.append(nome)
+        lista_pesados.append(peso)
+    else:
+        lista_leves.append(nome)
+        lista_leves.append(peso)
+        
     if res == 'N':
         print('ENCERRANDO . . .')
         break
-        
-    
     
 print(lista_usuarios)
+print(lista_leves)
+print(lista_pesados)
