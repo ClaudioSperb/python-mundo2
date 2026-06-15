615ae472f40d5fcbf7e8313e31fef4e484334e25
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-15 08:26:06 -0300

    Update desafio084.py

--- Desafios/desafio084.py
+++ Desafios/desafio084.py
@@ -1,3 +1,17 @@
 print(40 * '=')
 print(f'LISTAGEM DE PESOS'.center(40))
 print(40 * '=')
+dados = []
+pessoa = []
+while True:
+    pessoa.append(str(input('Nome: ')))
+    pessoa.append(str(input('Peso: ')))
+    res = str(input('Quer continuar [S / N]? ')).upper().strip()
+    
+    dados.append(pessoa.copy())
+    pessoa.clear()
+    
+    if res == 'N':
+        break
+    
+print(dados)
