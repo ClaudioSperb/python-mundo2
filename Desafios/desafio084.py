d43fdc69ece0850f7908db11951f4036e45cb3df
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-15 13:29:10 -0300

    Update desafio084.py

--- Desafios/desafio084.py
+++ Desafios/desafio084.py
@@ -3,15 +3,37 @@ print(f'LISTAGEM DE PESOS'.center(40))
 print(40 * '=')
 dados = []
 pessoa = []
+lista_leve = []
+lista_pesado = []
+
 while True:
-    pessoa.append(str(input('Nome: ')))
-    pessoa.append(str(input('Peso: ')))
+    nome = str(input('Nome: '))
+    peso = float(input('Peso: '))
     res = str(input('Quer continuar [S / N]? ')).upper().strip()
     
+    pessoa.append(nome)
+    pessoa.append(peso)
     dados.append(pessoa.copy())
     pessoa.clear()
     
+    
+    if peso <= 70:
+        lista_leve.append(nome)
+        lista_leve.append(peso)
+    else:
+        lista_pesado.append(nome)
+        lista_pesado.append(peso)
+        
+
     if res == 'N':
         break
+
+# for p, n in enumerate(lista_leve):
+#     print(n,p)
     
-print(dados)
+# for n, p in enumerate(lista_pesado):
+#     print(n,p)
+  
+print(f'No total, temos {len(dados)}pessoas cadastradas. ')
+print(f'Lista de pessoas a baixo de 70Kg >>>> {lista_leve}')
+print(f'Lista de pessoas a cima de 70Kg >>>> {lista_pesado}')
