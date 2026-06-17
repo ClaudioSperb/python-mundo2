c7e716b843d5da71120a773b1713dfce3eb12cc3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-17 16:20:41 -0300

    Create desafio086.py

---
+++ Desafios/desafio086.py
@@ -0,0 +1,38 @@
+from colorama import Fore
+from time import sleep
+
+print(40 * '=')
+print(f'MATRIZ'.center(40))
+print(40 * '=')
+
+lista_1 = []
+lista_2 = []
+lista_3 = []
+
+for n in range(1):
+    num1 = int(input('Digite um número para a posição [0, 0]: '))
+    num2 = int(input('Digite um número para a posição [0, 1]: '))
+    num3 = int(input('Digite um número para a posição [0, 2]: '))
+    lista_1.append(num1)
+    lista_1.append(num2)
+    lista_1.append(num3)
+    
+for n in range(1):
+    num1 = int(input('Digite um número para a posição [1, 0]: '))
+    num2 = int(input('Digite um número para a posição [1, 1]: '))
+    num3 = int(input('Digite um número para a posição [1, 2]: '))
+    lista_2.append(num1)
+    lista_2.append(num2)
+    lista_2.append(num3)
+    
+for n in range(1):
+    num1= int(input('Digite um número para a posição [2, 0]: '))
+    num2 = int(input('Digite um número para a posição [2, 1]: '))
+    num3 = int(input('Digite um número para a posição [2, 2]: '))
+    lista_3.append(num1)
+    lista_3.append(num2)
+    lista_3.append(num3)
+
+print(lista_1)
+print(lista_2)
+print(lista_3)
