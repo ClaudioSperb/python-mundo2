1955aed12caa114fefaf5400ced1f681c41458f2
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-19 16:14:13 -0300

    Update desafio089.py

--- Desafios/desafio089.py
+++ Desafios/desafio089.py
@@ -2,18 +2,27 @@ print(40 * '=')
 print(f'< BOLETIM ESCOLAR >'.center(40))
 print(40 * '=')
 
-dados_alunos = []
 
 while True:
     nome = str(input('Nome: ')).capitalize().strip()
     nota_1 = float(input('Nota 1: '))
-    nome_2 = float(input('Nota 2: '))
+    nota_2 = float(input('Nota 2: '))
     res = str(input('Quer continuar [S / N]: ')).capitalize().strip()
+    media_nota = (nota_1 + nota_2 ) / 2
+    
+    dados_alunos = [nome,[nota_1, nota_2], [media_nota] ]
     dados_alunos.append(nome)
     dados_alunos.append(nota_1)
-    dados_alunos.append(nome_2)
+    dados_alunos.append(nota_2)
+    dados_alunos.append(media_nota)
+    
     
     if res == 'N':
         break
     
+
 print(dados_alunos)
+for n in dados_alunos:
+    print(dados_alunos[0])
+    print(dados_alunos[1])
+    print(dados_alunos[2])
