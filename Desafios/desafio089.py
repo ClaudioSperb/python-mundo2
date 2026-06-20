f8a352a58fb82f78afdb91929498c7b5b95f2786
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-20 08:35:13 -0300

    Update desafio089.py

--- Desafios/desafio089.py
+++ Desafios/desafio089.py
@@ -25,3 +25,11 @@ print(f'{"Nº.":<4}{"NOME":<10}{"MÉDIA":>8}')
 for p , n in enumerate(lista_geral):
     print(f'{p:<4}{n[0]:<10}{n[2]:>8.1f}')
     print(30 * '-')
+
+while True:
+    aluno = int(input('Mostrar notas de qual aluno [ 999 ] para sair]: '))
+    for n, a in enumerate(dados_alunos):
+        if n == aluno:
+            print(f'A nota do alunoª {dados_alunos[0]} >>> {dados_alunos[1]}')
+    if aluno == 999:
+        break
