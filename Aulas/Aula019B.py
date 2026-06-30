182677f630dbdeaf7a4fb53ccb114d7f80f2edc9
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-30 16:11:52 -0300

    Update Aula019B.py

--- Aulas/Aula019B.py
+++ Aulas/Aula019B.py
@@ -1,11 +1,14 @@
-print('Iterando com Dicionários')
-estados = []
-uf_sigla = dict()
+print('Adicionando direto em listas dentro do Dicionário')
+# Inicializamos o dicionário já com as chaves e listas vazias
+estados = {
+    'uf': [],
+    'sigla': []
+}
+
 for c in range(3):
-    uf_sigla ['uf']= str(input('UF: ')).upper()
-    uf_sigla ['sigla']= str(input('Sigla: ')).upper()
-    
-    estados.append(uf_sigla.copy())
-for e in uf_sigla:
-    for c,v in uf_sigla.items():
-        print(c, v)
+    # Dá um .append() direto na lista interna correspondente à chave
+    estados['uf'].append(str(input('UF: ')).upper())
+    estados['sigla'].append(str(input('Sigla: ')).upper())
+
+print("\nDicionário final:")
+print(estados)
