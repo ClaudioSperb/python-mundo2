b14212b76fe60a36251194b4226d5c9a5b803192
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-03 13:54:08 -0300

    Update desafio094.py

--- Desafios/desafio094.py
+++ Desafios/desafio094.py
@@ -9,7 +9,7 @@ lista_geral = []
 cadastro_pessoas = {}
 
 while True:
-    cadastro_pessoas['nome'] = str(input('Nome: '))
+    cadastro_pessoas['nome'] = str(input('Nome: ')).upper().strip()
     cadastro_pessoas['sexo'] = str(input('Sexo: [M / F] ')).upper().strip()
     cadastro_pessoas['idade'] = int(input('Idade: '))
     res = str(input('Quer continuar? [S / N] ')).upper().strip()
@@ -23,7 +23,31 @@ while True:
 tot_pessoas = len(lista_geral)
 
 #Somando as idades totais da lista geral
+soma_idade = 0
+for idade in lista_geral:
+    soma_idade += idade['idade']
 
-print(tot_pessoas)
+#Pegando a media da idade das pessoas cadastradas
+media_idade = soma_idade / tot_pessoas
+
+print(30 * '-=')
 print(lista_geral)
+print(30 * '-=')
 
+print(f'Foram cadastradas {tot_pessoas} pessoas no total.')
+print(f'A média de idade das pessoas cadastradas foi de {media_idade:.1f} anos')
+    
+print(f'As mulheres cadastradas foram: ', end=' ')
+for pessoa in lista_geral:
+    if pessoa['sexo'] == 'F':
+        print(pessoa['nome'], end=' ')
+print()
+
+print(30 * '-=')
+
+print('Lista das pessoas com idade acima da média:')
+for pessoa in lista_geral:
+    if pessoa['idade'] >= media_idade:
+        print(f'{pessoa['nome']} -> {pessoa['idade']} anos -> {pessoa['sexo']}')
+        print(30 * '~~')
+print('FIM DO CADASTRO - VOLTE SEMPRE')
