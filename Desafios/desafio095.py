f3c40779cd0a13604689994e2d86eaa06b756413
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-03 15:36:49 -0300

    Create desafio095.py

---
+++ Desafios/desafio095.py
@@ -0,0 +1,25 @@
+print(30 * '=')
+print(f'ESTATÍSTICAS DE UM JOGADOR'.center(30))
+print(30 * '=')
+
+estatisticas_jogador = {
+}
+gols_jogadores = []
+#Pegando os nomes dos jogadores
+while True:
+    estatisticas_jogador['nome'] = str(input('Nome do jogador: ')).upper().strip()
+    estatisticas_jogador['partidas'] = int(input(f'Quantas partidas {estatisticas_jogador["nome"]} jogou: '))
+    if estatisticas_jogador['partidas'] > 0:
+        for c in range(estatisticas_jogador['partidas']):
+            estatisticas_jogador['gols_na_partida'] = int(input(f'Quantos gols na {c + 1}ª partida: '))
+            gols_jogadores.append(estatisticas_jogador['gols_na_partida'])
+            gols_jogadores.copy()
+            
+    res = str(input('Quer continuar? S / N ')).upper()
+    if res == 'N':
+        break
+print(estatisticas_jogador)
+print(gols_jogadores)
+    
+    
+            
