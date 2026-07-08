2fa86ec0b0cdb31f570156786c69794937a4642c
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-08 13:15:30 -0300

    Update desafio093b.py

--- Desafios/desafio093b.py
+++ Desafios/desafio093b.py
@@ -5,32 +5,34 @@ print(30 * '=')
 
 jogador = {}
 partidas = []
-
-jogador['nome'] = str(input('Nome do jogador: ')).capitalize()
-tot = int(input(f'Quantas partidas {jogador["nome"]} Jogou: '))
-
-for c in range(tot):
-    partidas.append(int(input(f'Gols na partida {c + 1}: ')))
-    
-jogador['gols'] = partidas.copy()
-jogador['total'] = sum(partidas)
-
+time = []
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: ')).capitalize()
+    tot = int(input(f'Quantas partidas {jogador["nome"]} Jogou: '))
+    partidas.clear()
+    for c in range(tot):
+        partidas.append(int(input(f'Gols na partida {c + 1}: ')))
+        
+    jogador['gols'] = partidas.copy()
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar [S / N]: ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
 print('=-' * 30)
-print(jogador)
-print('=-' * 30)
-
-for k, v in jogador.items():
-    print(f'No campo {k} tem o valor {v}')
-    sleep(0.5)
-    
-print('=-' * 30)
-
-print(f'O jogador {jogador["nome"]}, jogou no total {len(jogador['gols'])} partidas.')
-
-print('=-' * 30)
-
-for i, v in enumerate(partidas):
-    print(f'     => Na partida {i + 1} fez {v} gols')
-    sleep(0.5)
-print(f'No total o jogador {jogador['nome']} fez {jogador['total']} gols.')
-print('')
+print('cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
