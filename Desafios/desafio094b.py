ce0d774d88b7c21d135a62addbc69b6232021465
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-08 07:16:50 -0300

    Update desafio094b.py

--- Desafios/desafio094b.py
+++ Desafios/desafio094b.py
@@ -5,48 +5,21 @@ print(30 * '=')
 cadastro_geral = []
 pessoa = dict()
 
-
 while True:
-    pessoa['nome'] = (str(input('Nome: ')).upper().strip())
-    pessoa['sexo'] = (str(input('Sexo: ')).upper().strip())
-    pessoa['idade'] = (int(input('idade: ')))
-    res = str(input('Quer continuar [S / N]: ')).upper().strip()
-    
-    cadastro_geral.append(pessoa.copy())
-    
-    
-    if res == 'N':
-        break
-    
-total_pessoas = len(cadastro_geral)
-
-tot_idade = 0
-
-for i, v in enumerate(cadastro_geral):
-    tot_idade += v['idade']
-    
-media_idade = tot_idade / total_pessoas
-
+    pessoa['nome'] = str(input('Nome: ')).upper().strip()
+    while True:
+        pessoa['sexo'] = str(input('Sexo [M / F]: ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('Opção inválida ! Responda com M ou F. Tente novamente.')
+    pessoa['idade'] = int(input('Idade: '))
+    while True:
+        resp = str(input('Quer continuar [S / N]: ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('Opção inválida. Responda S ou N. Tente Novamente')
         
-print('=-' * 30)
-print(cadastro_geral)
-print('=-' * 30)
-print(f'No total foram {total_pessoas} cadastradas.')
-print(f'A media de idade das pessoas cadastradas é {media_idade:.1f} anos.')
-print('=-' * 30)
-#pegando somente as Mulheres
-print('As mulheres cadastradas foram: ', end=' ')
-for i, v in enumerate(cadastro_geral):
-    if v['sexo'] == 'F':
-        print(f'{v['nome']}', end=' ')
-print('')
-print('=-' * 30)
-
-print('Pessoa com idade a cima da média => ', end=' ')
-for c in cadastro_geral:
-    if c['idade'] >= media_idade:
-        print(c['nome'], end=' ')
-print()
+print(pessoa)
 print('=-' * 30)
 print('FIM DO CADASTRO'.center(60))
 print()
