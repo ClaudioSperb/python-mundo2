35834044cb3561cc947688e933a7bb3b064dcc24
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-09 14:36:32 -0300

    Update aula020 - funções.py

--- Aulas/aula020 - funções.py
+++ Aulas/aula020 - funções.py
@@ -1,11 +1,16 @@
 def area_triangulo(x, y, z):
     a = (x * y) / z
-    print(f'A área total do triângulo é {a}')
+    print('=-' * 30)
+    print(f'A área total do triângulo é {a}'.center(60))
+    print('=-' * 30)
+while True:
+    x = float(input('Digite o valor para X: '))
+    y = float(input('Digite o valor para Y: '))
+    z = float(input('Digite o valor para Z: '))
+    
+    area_triangulo(x, y, z)
+    
+    res = str(input('Quer continuar: S / N ')).upper()[0]
 
-x = float(input('Digite o valor para X: '))
-y = float(input('Digite o valor para Y: '))
-z = float(input('Digite o valor para Z: '))
-
-print(f'Voce digitou {x}, {y} e {z}.', end=' ')
-
-area_triangulo(x, y, z)
+    if res == 'N':
+        break
