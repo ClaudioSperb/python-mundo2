158d19a6a30027f8bedaff302076d569dff82c15
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-09 14:28:56 -0300

    Update aula020 - funções.py

--- Aulas/aula020 - funções.py
+++ Aulas/aula020 - funções.py
@@ -2,9 +2,9 @@ def area_triangulo(x, y, z):
     a = (x * y) / z
     print(f'A área total do triângulo é {a}')
 
-x = float(input('Digite o valor: '))
-y = float(input('Digite o valor: '))
-z = float(input('Digite o valor: '))
+x = float(input('Digite o valor para X: '))
+y = float(input('Digite o valor para Y: '))
+z = float(input('Digite o valor para Z: '))
 
 print(f'Voce digitou {x}, {y} e {z}.', end=' ')
 
