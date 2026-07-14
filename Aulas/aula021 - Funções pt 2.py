8b6cbb0c79af083520f57ea99c28b2046d6f2ed4
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-14 14:59:32 -0300

    Update aula021 - Funções pt 2.py

--- Aulas/aula021 - Funções pt 2.py
+++ Aulas/aula021 - Funções pt 2.py
@@ -1,4 +1,13 @@
 #MAIS SOBRE FUNÇÕES - AULA 21
+#INTERACTIVE HELP -
+'''
+1. Interactive Help (help())
+É uma ferramenta embutida no Python que acessa a documentação de qualquer objeto, função ou módulo diretamente no terminal.
+
+Explicação: Ao digitar help(objeto), o Python abre um manual explicando o que aquele item faz.
+
+'''
+
 from colorama import Fore
 help(Fore)
 help(print)
