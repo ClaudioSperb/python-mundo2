678c1ed362cbc8ab06d3e341d148639238fdbb39
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-17 14:07:19 -0300

    Create desafio102.py

---
+++ Desafios/desafio102.py
@@ -0,0 +1,18 @@
+from colorama import Fore
+
+def titulo(palavra):
+    tam = len(palavra)
+    print('=-'* tam)
+    print(f'{palavra}'.center(tam * 2))
+    print('=-'* tam)
+titulo('FATORIAL')
+
+#Criando uma função que pega o Fatorial usando o FOR.
+def fatorial(num = 1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é igual {fatorial(n)}')
