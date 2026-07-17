86efe631992c83cfcd0d5b8985d9a419a4e23814
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-17 16:59:14 -0300

    Update desafio102.py

--- Desafios/desafio102.py
+++ Desafios/desafio102.py
@@ -8,7 +8,7 @@ def titulo(palavra):
 titulo('FATORIAL')
 
 #Criando uma função que pega o Fatorial usando o FOR.
-def fatorial(num = 1):
+def fatorial(num = 1, show=False):
     f = 1
     for c in range(num, 0, -1):
         f *= c
