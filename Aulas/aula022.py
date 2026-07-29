759b2255d4107a5dd49ad289ff1f97c0d115b9a7
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-07-29 13:59:26 -0300

    atualização

---
+++ Aulas/aula022.py
@@ -0,0 +1,15 @@
+'''
+1. Conceito Básico: Qual a diferença?
+Módulo: É simplesmente um arquivo .py contendo funções, classes e variáveis que você pode importar em outros arquivos.
+
+Pacote: É um diretório (pasta) que agrupa vários módulos relacionados. É como uma caixa organizada de ferramentas.
+'''
+
+'''
+1. O Conceito Fundamental (A Analogia)
+Módulo: É simplesmente um arquivo .py. Qualquer arquivo Python com funções, variáveis ou classes é um módulo.
+
+Pacote: É uma pasta que agrupa vários módulos (.py) relacionados.
+
+A regra de ouro: Se o seu código está ficando longo demais em um único arquivo, divida-o em módulos!
+'''
