d80aae68aa92dd1749d5e3b79fa0aa8731db69a5
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-08-06 11:39:39 -0300

    atualização

--- Desafios/desafio114.py
+++ Desafios/desafio114.py
@@ -4,8 +4,9 @@ import urllib.error
 from colorama import Fore
 
 try:
-    site = urllib.request.urlopen('http://facebook.com/')
+    site = urllib.request.urlopen('http://www.pudim.com.br/')
 except urllib.error.URLError:
     print(f'{Fore.RED}O site não está acessível no momento.{Fore.RESET}')
 else:
     print(f'{Fore.GREEN}Consegui acessar o site com sucesso!{Fore.RESET}')
+    print(site.read())
