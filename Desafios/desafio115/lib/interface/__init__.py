0f197cba5746291cbe0e2b99765215bba874781c
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-08-06 13:13:53 -0300

    atualização

--- Desafios/desafio115/lib/interface/__init__.py
+++ Desafios/desafio115/lib/interface/__init__.py
@@ -1,7 +1,33 @@
+from colorama import Fore
+def validando_numero_inteiro(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except InterruptedError:
+            print('O Usuário não informou um dos valores solicitados')
+        except(TypeError, ValueError):
+            print(f'{Fore.LIGHTRED_EX}[ERRO]: Por favor digite um valor inteiro válido{Fore.RESET}')
+        except KeyboardInterrupt:
+            print(f'{Fore.RED}Usuario nao informou algum dos campos do sistema!{Fore.RESET}')
+            return 0
+        else:
+            return n
+
 def linha(tam=42):
     return '-' * tam
 
-def cabeçalho(txt):
+def cabecalho(txt):
     print(linha())
-    print(txt)
+    print(f'{txt}'.center(42))
     print(linha())
+
+
+def menu(lista):
+    cabecalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c += 1
+    print(linha())
+    opc = validando_numero_inteiro(f'\033[32mSua opção: \033[m')
+    return opc
---
+++ Desafios/desafio115/sistema.py
@@ -0,0 +1,15 @@
+from Desafios.desafio115.lib.interface import *
+from time import sleep
+
+while True:
+    resposta = menu(['Relatorio de Cadastro', 'Novo Cadastro' ,'Sair do Sistema'])
+    if resposta == 1:
+        cabecalho('Opção 1')
+    elif resposta == 2:
+        cabecalho('Opção 2')
+    elif resposta == 3:
+        cabecalho('Encerrando o Sistema - Volte Sempre!')
+        break
+    else:
+        print(f'\033[31m[ERRO]! - Digite uma opção válida!\033[m')
+    sleep(2)
