ee0fcbe118452a6f00f403b0a56e230c7cc5a5fb
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-08-07 15:06:04 -0300

    ATUALIZAÇÃO

--- Desafios/desafio115/sistema.py
+++ Desafios/desafio115/sistema.py
@@ -4,9 +4,9 @@ from time import sleep
 while True:
     resposta = menu(['Relatorio de Cadastro', 'Novo Cadastro' ,'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opção 1')
+        cabecalho('Opção 1 - \033[34mRELATÓRIO DE CADASTRO\033[m')
     elif resposta == 2:
-        cabecalho('Opção 2')
+        cabecalho('Opção 2 - \033[34mNOVO CADASTRO\033[m')
     elif resposta == 3:
         cabecalho('Encerrando o Sistema - Volte Sempre!')
         break
